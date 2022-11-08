efef3b12a6c53b7a9e8222c769a4522e1dac6064
Seb Beattie <sebastian.beattie@bbc.co.uk>
2022-11-08 14:06:46 +0000

    Modified the datashmace tool

--- addStuffToDatabase.js
+++ addStuffToDatabase.js
@@ -2,8 +2,8 @@ const db = require("better-sqlite3")("QuizData.db");
 const prompt = require('prompt-sync')();
 
 const topic = prompt("Enter a topic: ");
-const questionjson = prompt("Enter the JSON array with the question data: ");
+const questionjson = JSON.parse(prompt("Enter the JSON array with the question data: "));
 
-db.prepare("INSERT INTO QuizData VALUES (?, ?)").run(topic, questionjson);
+db.prepare("INSERT INTO QuizData VALUES (?, ?)").run(topic, JSON.stringify(questionjson, null, 1));
 
 console.log("Done diddly done");
