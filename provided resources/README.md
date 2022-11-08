4f62c7bbfda2121fd9ceaf943b1c3596e048249b
Daniel Garwood <daniel.garwood@bbc.co.uk>
2022-11-08 12:25:17 +0000

    removed provided resources

--- provided resources/README.md
+++
@@ -1,1 +0,0 @@
-# the-best-academy-project-ever
--- provided resources/jsonclient.py
+++
@@ -1,8 +0,0 @@
-import http.client
-import json
-
-conn = http.client.HTTPConnection('localhost', 4000)
-conn.request('GET', '/return', '{}')
-mydata = conn.getresponse().read()
-print("Python client running")
-print(json.loads(mydata))
