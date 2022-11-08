b0ffaf0cc9c08e878f525aaf2185c318068d25f7
Seb Beattie <sebastian.beattie@bbc.co.uk>
2022-11-08 14:14:54 +0000

    and again

--- addStuffToDatabase.js
+++ addStuffToDatabase.js
@@ -4,6 +4,6 @@ const prompt = require('prompt-sync')();
 const topic = prompt("Enter a topic: ");
 const questionjson = JSON.parse(prompt("Enter the JSON array with the question data: "));
 
-db.prepare("INSERT INTO QuizData VALUES (?, ?)").run(topic, JSON.stringify(questionjson, null, 1));
+db.prepare("INSERT INTO QuizData VALUES (?, ?)").run(topic, JSON.stringify(questionjson));
 
 console.log("Done diddly done");
