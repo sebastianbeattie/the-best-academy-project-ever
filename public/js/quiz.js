7e435f4d1ca1294c0029b9ad8c3aab03702f3eec
Daniel Garwood <daniel.garwood@bbc.co.uk>
2022-11-09 13:23:07 +0000

    removed plus button

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -88,7 +88,7 @@ function addTopicToUi(topic) {
             <div class="col-md-6 col-lg-4 mb-5">
                 <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal${index}${topicName}">
                 <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                <div class="portfolio-item-caption-content text-center text-white"><i"></i></div>
                 </div>
                 <h2 class="page-section-heading text-secondary mb-0 d-inline-block">Question ${index + 1}</h2>
                 </div>
